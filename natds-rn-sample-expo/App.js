ddc9b2d09a823214984f22bbb804ea9d0e9b8c41
Ariel Flor <arielwb@gmail.com>
2020-06-10 19:42:30 -0300

    chore: fix ThemeProvider context issue, fix fonts for web
    DSY-778

--- natds-rn-sample-expo/App.js
+++ natds-rn-sample-expo/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Platform } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import * as Font from 'expo-font';
+import { ThemeProvider } from 'styled-components/native';
 import { tokens } from '@naturacosmeticos/natds-styles';
 import { buildTheme } from '@naturacosmeticos/natds-rn';
 import { Button } from '@naturacosmeticos/natds-rn';
 
 export default function Main() {
-  const theme = buildTheme(tokens, 'avon', 'light');
+  const theme = buildTheme('avon', 'light');
+  const [isFontLoaded, fontsLoaded] = useState(false);
+
+  // load fonts to preview this snack on the browser correctly
+  loadFontsForWeb(isFontLoaded, fontsLoaded);
 
   return (
     <ThemeProvider theme={theme}>
@@ -26,6 +32,20 @@ const App = () => {
   );
 }
 
+const loadFontsForWeb = async (isFontLoaded, fontsLoaded) => {
+  // natds-rn is uses the standard system font for ios and android
+  // but in the browser we dont have those fonts available so have to install it
+
+  if (Platform.OS === 'web' && !isFontLoaded) {
+    const something = await Font.loadAsync({
+      'Roboto-Bold': require('./node_modules/@naturacosmeticos/natds-rn/build/lib/assets/fonts/Roboto-Bold.ttf'),
+      'Roboto-Regular': require('./node_modules/@naturacosmeticos/natds-rn/build/lib/assets/fonts/Roboto-Regular.ttf'),
+    });
+  }
+
+  fontsLoaded(true);
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -33,7 +53,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button:{
+  button: {
     marginBottom: 10
   }
 });
